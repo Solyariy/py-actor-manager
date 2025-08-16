4c007431a64afa5745d7e838cf29d4e9720b9d28
Oleksandr Skoromnov <solalex005@gmail.com>
2025-08-16 23:11:45 +0300

    solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,44 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self._connection = sqlite3.connect(db_name)
+        self.table_name = table_name
+
+    def all(self) -> list[Actor]:
+        cursor = self._connection.execute(
+            f"select * from {self.table_name}"
+        )
+        return [
+            Actor(*row)
+            for row in cursor
+        ]
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"insert into {self.table_name} "
+            f"(first_name, last_name) values"
+            f"(?, ?)",
+            (first_name, last_name)
+        )
+
+    def delete(self, pk: int) -> None:
+        self._connection.execute(
+            f"delete from {self.table_name} "
+            f"where id = ?",
+            (pk,)
+        )
+
+    def update(
+            self,
+            pk: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"update {self.table_name} "
+            f"set first_name = ?, last_name = ? "
+            f"where id = ?",
+            (new_first_name, new_last_name, pk)
+        )
